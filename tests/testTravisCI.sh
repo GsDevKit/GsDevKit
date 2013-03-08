d224963a9628b5ff721d8e6a379a8fe34ac1b9b5
Dale Henrichs <dhenrich@vmware.com>
2013-03-07 18:01:31 -0800

    got all the basic pieces assembled to trigger GLASS builds on travisCI, methinks

--- tests/testTravisCI.sh
+++ tests/testTravisCI.sh
@@ -3,53 +3,45 @@
 #  GLASS test driver for running GLASS builds on travisCI ... for now the code
 #    is hosted elsewhere, but ultimately the packages will be managed here.
 #
+#  This boyo generates an old-style configuration load script similar to those 
+#    used by GemTools and builderCI in before_gemstone.sh, but we're testing
+#    a barebones load and test so no need for Metacello Scripting API, to be
+#    pre-installed.
+#
 #      -verbose flag causes unconditional transcript display
 #
 # Copyright (c) 2013 VMware, Inc. All Rights Reserved <dhenrich@vmware.com>.
 #
 
-echo "--->$TRAVIS_BUILD_DIR"
-echo "`pwd`"
-
-if [ "${CONFIGURATION}x" = "x" ]; then
-  if [ "${BASELINE}x" = "x" ]; then
-    echo "Must specify either BASELINE or CONFIGURATION"
-    exit 1
-  else
-    PROJECT_LINE="  baseline: '${BASELINE}';"
-    VERSION_LINE=""
-    FULL_CONFIG_NAME="BaselineOf${BASELINE}"
-  fi
-else
-  PROJECT_LINE="  configuration: '${CONFIGURATION}';"
-  VERSION_LINE="  version: '$VERSION';"
-  FULL_CONFIG_NAME="ConfigurationOf${CONFIGURATION}"
-fi
-
-if [ "${REPOSITORY}x" = "x" ]; then
-  echo "Must specify REPOSITORY"
-  exit 1
-fi
-REPOSITORY_LINE="  repository: '$REPOSITORY';"
-
-OUTPUT_PATH="${PROJECT_HOME}/tests/travisCI.st"
+OUTPUT_PATH="${PROJECT_HOME}/tests/glass.st"
 
 cat - >> $OUTPUT_PATH << EOF
- Transcript cr; show: 'travis--->${OUTPUT_PATH}'.
- "Load the configuration or baseline"
- Metacello new
- $PROJECT_LINE
- $VERSION_LINE
- $REPOSITORY_LINE
-   load: #( ${LOADS} ).
-  "Run the tests"
-  TravisCIHarness
-    value: #( '${FULL_CONFIG_NAME}' )
-    value: 'TravisCISuccess.txt' 
-    value: 'TravisCIFailure.txt'.
+Transcript show: 'travis--->${OUTPUT_PATH}'
+ConfigurationOfGLASS project updateProject.
+[
+(ConfigurationOfGLASS project version: '${VERSION}') load: #( ${INITIAL_LOADS} ).
+(ConfigurationOfGLASS project version: '${VERSION}') load ]
+    on: Warning
+    do: [:ex |
+        Transcript cr; show: 'Warning: ', ex description.
+        ex resume: true].
 EOF
 
 cat $OUTPUT_PATH
 
-$BUILDER_CI_HOME/testTravisCI.sh "$@"
+# Inline $BUILDER_CI_HOME/testTravisCI.sh so I can include -G option, etc.
+./build.sh -i $ST -G -f "$PROJECT_HOME/tests/before_gemstone.st" -f "$PROJECT_HOME/tests/glass.st" -m -f "$PROJECT_HOME/tests/runGlassTests.st" -o travisCI
+if [[ $? != 0 ]] ; then
+  echo "ERROR: $(basename $0)"
+  cd "${BUILD_PATH}/travisCI/"
+  $BUILDER_CI_HOME/buildImageErrorCheck.sh # dump Transcript on error and exit
+  if [[ $? != 0 ]] ; then exit 1; fi
+  $BUILDER_CI_HOME/dumpTranscript.sh
+  exit 1
+fi
+cd "${BUILD_PATH}/travisCI/"
+$BUILDER_CI_HOME/buildImageErrorCheck.sh # dump Transcript on error and exit
 if [[ $? != 0 ]] ; then exit 1; fi
+$BUILDER_CI_HOME/buildTravisStatusCheck.sh "$@" # dump Transcript on failed tests and exit
+if [[ $? != 0 ]] ; then exit 1; fi
+~ 
